630c570fc0b7900de1d4d36fbb6e24bbf83cb49e
Jhosep2022 <114316578+Jhosep2022@users.noreply.github.com>
2023-04-13 12:08:45 -0400

    Agregacion de botones y imagenes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://imagenes.20minutos.es/files/og_thumbnail/uploads/imagenes/2021/04/22/el-perro-picado-por-una-abeja.jpeg">
+    <img class="image" alt="Vue logo" src="https://imagenes.20minutos.es/files/og_thumbnail/uploads/imagenes/2021/04/22/el-perro-picado-por-una-abeja.jpeg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.image{
+    width: 100%;
+    height: 25%;
 }
 </style>
